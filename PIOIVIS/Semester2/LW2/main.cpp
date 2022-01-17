7fd47f752bda90b0aeffd057fa92efb653648f1f
Rostislav Lipsky <ungaf@ya.ru>
2022-01-17 16:19:02 +0300

    -O2 compiler flag

--- PIOIVIS/Semester2/LW2/main.cpp
+++ PIOIVIS/Semester2/LW2/main.cpp
@@ -1,11 +1,18 @@
 #include <fstream>
+#include <iostream>
 #include "Graph.h"
 
-int main() {
+int main(int argc, char ** argv) {
 
-    Graph myGraph("input.txt");
-    std::ofstream fout("output.txt");
-    fout << myGraph.getTreeDiameter();
+    if (argc == 1) {
+        Graph myGraph("input.txt");
+        std::ofstream fout("output.txt");
+        fout << myGraph.getTreeDiameter();
+        fout.close();
+    } else {
+        Graph myGraph(argv[1]);
+        std::cout << myGraph.getTreeDiameter() << std::endl;
+    }
     return 0;
 
 }
