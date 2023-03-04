b253618abd8afdb812eecd64ee6576c3efbc24c7
Rastsislau <ungaf@ya.ru>
2023-03-04 21:42:38 +0300

    sem4/lois/lw1 fixed precedence

--- semester-4/LOIS/LW1/system.py
+++ semester-4/LOIS/LW1/system.py
@@ -48,6 +48,15 @@ class Token(Enum):
         return self.name
 
 
+TOKEN_PRECEDENCE = {
+        Token.NEG: 3,
+        Token.AND: 2,
+        Token.OR: 1,
+        Token.IMP: 0,
+        Token.EQU: 0,
+}
+
+
 def get_tokens(raw: str):
     raw = raw.replace(" ", "")
 
@@ -85,6 +94,8 @@ def to_rpn(tokens):
         if token == Token.VAR:
             output.append(token)
         elif token in [Token.AND, Token.OR, Token.NEG, Token.IMP, Token.EQU]:
+            while stack and TOKEN_PRECEDENCE.get(token, 0) <= TOKEN_PRECEDENCE.get(stack[-1], 0):
+                output.append(stack.pop())
             stack.append(token)
         elif token == Token.OP_BRACE:
             stack.append(token)
