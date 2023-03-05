cf3d52889dbcdb7724eb71bfd9811472fcf03272
Rastsislau <ungaf@ya.ru>
2023-03-05 15:37:04 +0300

    sem4/lois/lw1 fix or in neg

--- semester-4/LOIS/LW1/dnf.py
+++ semester-4/LOIS/LW1/dnf.py
@@ -1,21 +1,47 @@
 from lexer import Token
 
 
+ATOMIC_EXPRESSIONS = [
+    Token.VAR,
+    Token.TRUE,
+    Token.FALSE
+]
+
+
+ALLOWED_OPS = [
+    Token.AND,
+    Token.VAR,
+    Token.TRUE,
+    Token.FALSE,
+    Token.NEG,
+    Token.OR
+]
+
+
 def is_and(node):
+    if node.value in ATOMIC_EXPRESSIONS:
+        return True
+
+    if node.value == Token.OR:
+        return False
+
     for child in [node.left, node.right]:
-        if child is not None and child.value not in [Token.AND, Token.VAR, Token.TRUE, Token.FALSE, Token.NEG, None]:
+        if child is not None and child.value not in [Token.AND, Token.VAR, Token.TRUE, Token.FALSE, Token.NEG]:
             return False
         if child is not None and child.value == Token.AND:
             if not is_and(child):
+                return False
+        if child is not None and child.value == Token.NEG:
+            if not is_and(child.left):
                 return False
     return True
 
 
 def is_dnf(node):
     if node.value == Token.NEG:
-        return node.left.value in [Token.VAR, Token.TRUE, Token.FALSE]
+        return is_and(node.left)
 
-    if node.value in [Token.VAR, Token.TRUE, Token.FALSE]:
+    if node.value in ATOMIC_EXPRESSIONS:
         return True
 
     if node.value == Token.AND:
@@ -25,7 +51,7 @@ def is_dnf(node):
         return False
 
     for child in [node.left, node.right]:
-        if child.value not in [Token.AND, Token.VAR, Token.TRUE, Token.FALSE, Token.NEG, Token.OR]:
+        if child.value not in ALLOWED_OPS:
             return False
         if child.value in [Token.OR, Token.AND]:
             if not is_dnf(child):
