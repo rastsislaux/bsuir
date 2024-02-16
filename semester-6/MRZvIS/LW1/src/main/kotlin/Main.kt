3ddea3f126991d0ca2de72ad373343d32bc8b7cb
Rastsislau Lipski <ungaf@ya.ru>
2024-02-16 13:30:42 +0300

    sem6/mrzvis/lw1 arithmetic pipeline

--- semester-6/MRZvIS/LW1/src/main/kotlin/Main.kt
+++ semester-6/MRZvIS/LW1/src/main/kotlin/Main.kt
@@ -1,20 +1,110 @@
-import by.bsuir.Number8
+/*
+Лабораторная работа #1 по предмету "Методы решения задач в интеллектуальных системах"
+
+Авторы:
+    студенты гр. 121701
+    Липский Р. В., Жолнерчик И. А., Стронгин А. В.
+
+Задание:
+    (16) реализовать алгоритм вычисления произведения пары 8-разрядных чисел умножением со старших разрядов
+         со сдвигом частичной суммы влево
+
+Источники:
+    (1) Интеграционная платформа
+    (2) Качинский, М. В. Арифметические основы электронных вычислительных средств : учебно-метод. пособие
+        / М. В. Качинский, В. Б. Клюс, А. Б. Давыдов. - Минск : БГУИР, 2014. - 64 с. : ил.
+ */
+
+import by.bsuir.*
 import java.lang.Exception
+import java.time.Duration
+import java.time.Instant
+import java.util.stream.IntStream
+import kotlin.streams.toList
 
 fun lwMain() {
-    print("Multiplicand: ")
-    val number1 = Number8(readln())
+    val pipeline = object: ArithmeticPipeline<MultiplicationTriple>(
+        MultiplicationStep(),
+        MultiplicationStep(),
+        MultiplicationStep(),
+        MultiplicationStep(),
+        MultiplicationStep(),
+        MultiplicationStep(),
+        MultiplicationStep(),
+        MultiplicationStep(),
+        MultiplicationStep(),
+    ) {
 
-    print("Factor: ")
-    val number2 = Number8(readln())
+        override fun postTick() {
+            println("===================== TICK $tick =====================")
+            steps.forEachIndexed { i, step ->
+                println("$i. Multiplicand: ${step.content?.multiplicand} | Factor: ${step.content?.factor} | Partial sum: ${step.content?.partialSum}")
+            }
+        }
 
-    print("$number1 + $number2 = ${number1 * number2}}")
+    }
+
+    val res = pipeline.run(
+        MultiplicationTriple(Number8(2), Number8(8),   Number8.ZERO),
+        MultiplicationTriple(Number8(6), Number8(6),   Number8.ZERO),
+        MultiplicationTriple(Number8(12), Number8(9),  Number8.ZERO),
+        MultiplicationTriple(Number8(15), Number8(15), Number8.ZERO),
+        MultiplicationTriple(Number8(14), Number8(7),  Number8.ZERO),
+        MultiplicationTriple(Number8(3), Number8(7),   Number8.ZERO)
+    )
+
+    res.forEach {
+        println(it)
+    }
 }
 
+class Benchmark(private val ops: Int) {
+
+    fun calculateSimple() {
+        for (i in 1..ops) {
+            val x = Number8(15) * Number8(15)
+        }
+    }
+
+    fun calculateParallel(list: List<MultiplicationTriple>) {
+        ArithmeticPipeline(
+            MultiplicationStep(),
+            MultiplicationStep(),
+            MultiplicationStep(),
+            MultiplicationStep(),
+            MultiplicationStep(),
+            MultiplicationStep(),
+            MultiplicationStep(),
+            MultiplicationStep(),
+            MultiplicationStep(),
+        ).run(*list.toTypedArray())
+    }
+
+    fun time(x: () -> Unit): Long {
+        val start = Instant.now().epochSecond
+        x()
+        val finish = Instant.now().epochSecond
+        return finish - start
+    }
+
+    fun run() {
+        val timeSimple = time { calculateSimple() }
+
+        val list = mutableListOf<MultiplicationTriple>()
+        for (i in 1..ops) {
+            list.add(MultiplicationTriple(Number8(15), Number8(15), Number8.ZERO))
+        }
+        val timeParallel = time { calculateParallel(list) }
+
+        println("Time Simple: ${timeSimple}s")
+        println("Time Parallel: ${timeParallel}s")
+    }
+
+}
 
 fun main() {
     try {
-        lwMain()
+        Benchmark(5000000).run()
     } catch (e: Exception) {
         println("Ошибка: ${e.message} (${e::class.qualifiedName})")
     }
---
+++ semester-6/MRZvIS/LW1/src/main/kotlin/by/bsuir/ArithmeticPipeline.kt
@@ -0,0 +1,93 @@
+/*
+Лабораторная работа #1 по предмету "Методы решения задач в интеллектуальных системах"
+
+Авторы:
+    студенты гр. 121701
+    Липский Р. В., Стронгин А. В., Жолнерчик И. А.
+
+Задание:
+    (16) реализовать алгоритм вычисления произведения пары 8-разрядных чисел умножением со старших разрядов
+         со сдвигом частичной суммы влево
+
+Источники:
+    (1) Интеграционная платформа
+    (2) Качинский, М. В. Арифметические основы электронных вычислительных средств : учебно-метод. пособие
+        / М. В. Качинский, В. Б. Клюс, А. Б. Давыдов. - Минск : БГУИР, 2014. - 64 с. : ил.
+ */
+
+package by.bsuir
+
+open class ArithmeticPipeline<T>(vararg steps: Step<T>) {
+
+    protected var tick: Int = 0
+    protected val steps: List<Step<T>>
+
+    init {
+        this.steps = steps.toList()
+    }
+
+    fun run(vararg input: T): List<T> {
+        val inputList = input.toMutableList()
+        val originalSize = inputList.size
+        val output = ArrayDeque<T>()
+
+        while (output.size != originalSize) {
+            tick(inputList, output)
+        }
+
+        return output
+    }
+
+    open fun preTick() { }
+
+    open fun postTick() { }
+
+    private fun tick(input: MutableList<T>, output: ArrayDeque<T>) {
+        tick++
+        preTick()
+        if (steps[steps.lastIndex].content != null) {
+            output.addFirst(steps[steps.lastIndex].content!!)
+            steps[steps.lastIndex].content = null
+        }
+
+        for (i in steps.lastIndex downTo 1) {
+            steps[i].content = steps[i - 1].content
+            steps[i - 1].content = null
+            steps[i].doWork()
+        }
+
+        if (input.isNotEmpty()) {
+            steps[0].content = input.removeLast()
+        }
+        postTick()
+    }
+
+}
+
+abstract class Step<T> {
+    var content: T? = null
+
+    fun doWork() {
+        if (content != null) {
+            content = doWork(content!!)
+        }
+    }
+
+    abstract fun doWork(input: T): T
+}
+
+data class MultiplicationTriple(
+    val multiplicand: Number8,
+    val factor: Number8,
+    val partialSum: Number8
+)
+
+class MultiplicationStep: Step<MultiplicationTriple>() {
+
+    override fun doWork(input: MultiplicationTriple): MultiplicationTriple {
+        val newPartialSum = (input.partialSum shl 1) + (if (input.factor[0]) input.multiplicand else Number8.ZERO)
+        val newFactor = input.factor shl 1
+        return MultiplicationTriple(input.multiplicand, newFactor, newPartialSum)
+    }
+
+}
